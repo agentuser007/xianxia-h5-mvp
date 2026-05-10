660d5f4bf899ff078c771d83b15714d653fbe4c4
agentuser007 <88312677+agentuser007@users.noreply.github.com>
2026-05-10 12:28:35 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  
+  base: '/xianxia-h5-mvp/', 
   plugins: [vue()],
-  server: {
-    port: 3000,
-    host: true
-  }
 })
+
